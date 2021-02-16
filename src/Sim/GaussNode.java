0d8c7ccccccdf2bc9d28fff52af814a1d59509ed
Olofsson Robin <roblof-8@student.ltu.se>
2021-02-16 14:16:20 +0100

    va händer

--- src/Sim/GaussNode.java
+++ src/Sim/GaussNode.java
@@ -6,12 +6,13 @@ public class GaussNode extends Node{
 	protected double _mean;
 	protected int sizeBetweenStaple;
 	protected int std_diviation;
+	protected double x;
 
 	public GaussNode(int network, int node, double _mean, int std_diviation) {
 		super(network, node);
 		this.std_diviation = std_diviation;
 		this._mean = _mean;
-
+		this.x = -(std_diviation * 2);
 	}
 
 
@@ -26,30 +27,28 @@ public class GaussNode extends Node{
 	}
 
 	public double calcNumberOfPackagesNB(double x){
-		return Math.exp(-Math.pow((x - _mean)/std_diviation,2)/2) / (std_diviation * Math.sqrt(2 * Math.PI));
+		return _stopSendingAfter * (Math.exp(-Math.pow((x - _mean)/std_diviation,2)/2) / (std_diviation * Math.sqrt(2 * Math.PI)));
 	}
 
 
 	// Override: Modified to send packages as a normal distribution with a random gaussian number.
 	public void recv(SimEnt src, Event ev)
 	{
-
-
 		if (ev instanceof TimerEvent)
 		{
 			if(_stopSendingAfter > _sentmsg) {
-				//int packages = calcNumberOfPackagesNB()
+				int packages = (int)calcNumberOfPackagesNB(x);
 
 
-				/*for (int i = 0; i < packages; i++) {
+				for (int i = 0; i < packages; i++) {
 
-					System.out.println("Time of sending package is: " + thisTime);
+					System.out.println("Time of sending package is: " + SimEngine.getTime());
 
 					_sentmsg++;
-					send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost), _seq), zeroOne);
+					send(_peer, new Message(_id, new NetworkAddr(_toNetwork, _toHost), _seq), 0);
 					System.out.println("Node " + _id.networkId() + "." + _id.nodeId() + " sent message with seq: " + _seq + " at time " + thisTime);
 					_seq++;
-				}*/
+				}
 				send(this, new TimerEvent(), 1);
 			}
 		}
